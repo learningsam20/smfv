95a8f3afa10cbeebbc825ecdd93d2479079a91cf
Sam <learningsam20@gmail.com>
2021-01-04 22:02:49 +0530

    Create trig.sh

---
+++ src/trig.sh
@@ -0,0 +1,6 @@
+ #! /bin/env bash
+  
+    #connect and execute python script. change password and use@host info
+    sshpass -p "password" ssh -o StrictHostKeyChecking=no user@mfip << PSCRIPT
+        python3 mtmlog.py
+    PSCRIPT
