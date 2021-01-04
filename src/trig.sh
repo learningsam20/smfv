3804318224857d21aec9a80bd12b12b011889e06
Sam <learningsam20@gmail.com>
2021-01-04 22:02:19 +0530

    Delete trig.sh

--- src/trig.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/env bash
-  
-    #connect and execute python script. change password and use@host info
-    sshpass -p "password" ssh -o StrictHostKeyChecking=no user@mfip << PSCRIPT
-        python3 mtmlog.py
-    PSCRIPT
-    
